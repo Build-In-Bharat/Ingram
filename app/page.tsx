b987c2dab89bf5a0d281a52e30d65c0cf9110714
Mohak Bajaj <bmohak87@gmail.com>
2024-09-02 18:14:00 +0530

    chore: Refactor file structure and update layout components

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "@/components/navbar/page";
+import First from "@/components/First/page";
+import Second from "@/components/second/page";
+import Third from "@/components/Third/page";
+import Fourth from "@/components/fourth/page";
+import Last from "@/components/last/page";
+import Fifth from "@/components/fifth/page";
+import Sixth from "@/components/sixth/page";
+import Footer from "@/components/footer/page";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Navbar />
+      <First />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Sixth />
+      <Last />
+      <Footer />
+    </main>
+  );
+}
