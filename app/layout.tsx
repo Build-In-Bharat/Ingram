081a07c79c2d5fee4e753254a9441aecefd26e68
aakash <mahajanaakash108@gmail.com>
2024-09-05 00:06:28 +0530

    GTM Tags Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify"; // Change Toaster to ToastContainer
 import 'react-toastify/dist/ReactToastify.css'; // Import CSS for Toastify
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T92N854K'); 
+            `,
+          }}
+        />
+      </Head>
       <body
         className={inter.className + " antialiased bg-background min-h-screen"}
       >
         <ToastContainer /> {/* Update Toaster to ToastContainer */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T92N854K"
+height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {children}
       </body>
-    </html>
+    </>
   );
 }
